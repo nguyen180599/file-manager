c24ebb89c5829715e3eee80f41f716d74e60d802
nguyen99 <pxnguyen180599@gmail.com>
2022-04-01 17:34:18 +0700

    abc

--- src/app/markdown/markdown.component.ts
+++ src/app/markdown/markdown.component.ts
@@ -1,5 +1,5 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {jsPDF} from "jspdf";
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { jsPDF } from "jspdf";
 import * as showdown from 'showdown'
 
 @Component({
@@ -8,11 +8,11 @@ import * as showdown from 'showdown'
   styleUrls: ['./markdown.component.scss']
 })
 export class MarkdownComponent implements OnInit {
-  @ViewChild('abc', {read: ElementRef}) abc!: ElementRef;
+  @ViewChild('abc', { read: ElementRef }) abc!: ElementRef;
   markdown = `## Markdown __rulez__!
 ---
 
-### Syntax highlight
+### Syntax highlight sdfsdf
 \`\`\`typescript
 const language = 'typescript';
 \`\`\`
@@ -36,21 +36,96 @@ const language = 'typescript';
     console.log(e.getContent());
   }
 
-  downloadMarkdown() {
-    console.log(this.abc.nativeElement.outerHTML);
-    const converter = new showdown.Converter();
-    const text = this.markdown;
-    const convertedMarkdown = converter.makeHtml(text);
-    console.log(convertedMarkdown);
+  createStorage(key: string) {
+    const store = JSON.parse(localStorage.getItem(key)!) ?? {}
 
-    const doc = new jsPDF();
-    // doc.setFontSize(10);
-    // doc.getFontSize();
-    doc.html(convertedMarkdown, {
-      callback: function (doc) {
-        doc.save('test.pdf');
+    const save = () => {
+      localStorage.setItem(key, JSON.stringify(store))
+    }
+
+    const storage = {
+      get(key: string) {
+        return store[key]
       },
-    });
-    // console.log(doc)
+      set(key: string, value: any) {
+        store[key] = value
+        save()
+      },
+      remove(key: string) {
+        
+      }
+    }
+  }
+
+  downloadMarkdown() {
+    console.log(this.abc.nativeElement.innerHTML);
+    console.log(this.markdown);
+
+    // const converter = new showdown.Converter();
+    // const text = this.markdown;
+    // this.convertedMarkdown = converter.makeHtml(text);
+    // console.log(this.convertedMarkdown);
+
+    // var doc = new jsPDF();
+
+    // doc.html(this.abc.nativeElement.outerHTML, {
+    //   callback: function (doc) {
+    //     // doc.setFontSize(15)
+    //     doc.save("test.pdf")
+    //   }
+    // });
+
+    const markdownHTML = `
+      <html>
+        <head>
+          <title></title>
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.27.0/themes/prism-okaidia.min.css" integrity="sha512-mIs9kKbaw6JZFfSuo+MovjU+Ntggfoj8RwAmJbVXQ5mkAX5LlgETQEweFPI18humSPHymTb5iikEOKWF7I8ncQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.27.0/plugins/line-highlight/prism-line-highlight.min.css" integrity="sha512-nXlJLUeqPMp1Q3+Bd8Qds8tXeRVQscMscwysJm821C++9w6WtsFbJjPenZ8cQVMXyqSAismveQJc0C1splFDCA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.27.0/plugins/line-numbers/prism-line-numbers.min.css" integrity="sha512-cbQXwDFK7lj2Fqfkuxbo5iD1dSbLlJGXGpfTDqbggqjHJeyzx88I3rfwjS38WJag/ihH7lzuGlGHpDBymLirZQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css" integrity="sha512-T584yQ/tdRR5QwOpfvDfVQUidzfgc2339Lc8uBDtcp/wYu80d7jwBgAxbyMh0a9YM9F8N3tdErpFI8iaGx6x5g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        </head>
+        <body>
+        <style>
+        img {max-width: 100vw;}
+        blockquote {
+          border-left: 3px solid rgba(0,0,0,0.21);
+          padding-left: 12px;
+          color: rgba(0,0,0,0.54);
+        }
+        @media print {
+          body {
+            -webkit-print-color-adjust:exact;
+          }
+          @print{
+            @page :footer {display: none }
+            @page :header {display: none}
+          }
+        }
+        pre {
+          word-break: break-all;
+          white-space: pre-wrap;
+          background: #272822;
+        }
+        pre code {
+          word-break: break-all;
+          white-space: pre-wrap !important;
+        }
+      </style>
+          ${this.abc.nativeElement.outerHTML}
+        </body>
+      </html>
+    `
+
+    // DOWNLOAD VỚI WINDOW PRINT
+    const myWindow = window.open('', 'PRINT', 'height=650,width=900,top=100,left=150');
+    myWindow?.document.write(markdownHTML);
+    myWindow?.document.close();
+    setTimeout(function () {
+      myWindow?.focus();
+      myWindow?.print();
+      myWindow!.onafterprint = () => {
+        myWindow?.close();
+      }
+    }, 1000)
   }
 }
